3349fcb34fc481830ae62b66514c8daacae55425
Arne De Smedt <arnedesmdt@gmail.com>
2018-12-21 16:07:04 +0100

    try catch to create a github repo

--- github.js
+++ github.js
@@ -7,14 +7,21 @@ module.exports = (api, options, rootOptions) => {
             token: options.githubToken,
         });
 
-        octokit.repos.createForAuthenticatedUser({
-            name: rootOptions.projectName,
-            description: options.description,
-            private: false,
-            has_projects: false,
-            has_wiki: false,
-            allow_squash_merge: false,
-            allow_rebase_merge: false,
-        });
+        try {
+            octokit.repos.createForAuthenticatedUser({
+                name: rootOptions.projectName,
+                description: options.description,
+                private: false,
+                has_projects: false,
+                has_wiki: false,
+                allow_squash_merge: false,
+                allow_rebase_merge: false,
+            });
+        } catch (e) {
+            /* eslint-disable no-console */
+            console.log(e);
+            console.log(`Could not create new gitlab project with name '${rootOptions.projectName}'.`)
+            /* eslint-enable no-console */
+        }
     }
 };
